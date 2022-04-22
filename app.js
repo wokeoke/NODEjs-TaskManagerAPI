4ace0dcffda69dec160d175d7b0edeec3f4a6ac5
wokeoke <woke.oke@gmail.com>
2022-04-23 05:48:28 +0700

    NOT FOUND

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
 
+const notFound = require('./middleware/not-found');
+
 // MIDDLEWARE
 app.use(express.static('./public'));
 app.use(express.json());
@@ -12,11 +14,7 @@ app.use(express.json());
 // ROUTES
 app.use('/api/v1/tasks', tasks);
 
-// app.get('/api/v1/tasks')          - get all the tasks
-// app.post('/api/v1/tasks')         - create a new task
-// app.get('/api/v1/tasks/:id')      - get single task
-// app.patch('/api/v1/tasks/:id')    - update task
-// app.delete('/api/v1/tasks/:id')   - delete task
+app.use(notFound);
 
 // PORT & CONNECT DB
 const PORT = 3000;
