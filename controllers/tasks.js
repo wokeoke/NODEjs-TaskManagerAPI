4902b9d2c82702ccda8070c665b63eebada78439
wokeoke <woke.oke@gmail.com>
2022-04-23 04:56:57 +0700

    getSingleTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -18,8 +18,19 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ message: `No Task With ID: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 const updateTask = (req, res) => {
