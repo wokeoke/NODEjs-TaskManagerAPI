49b41c003518e22e909e63e40d9bf5de4502e77b
wokeoke <woke.oke@gmail.com>
2022-04-23 05:05:57 +0700

    Delete Task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -37,8 +37,19 @@ const updateTask = (req, res) => {
   res.send('update task');
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ message: `No Task With ID: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 module.exports = {
