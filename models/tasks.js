0d7ebd0992c5a1d41b27a73a0d189bbe71ab64a1
wokeoke <woke.oke@gmail.com>
2022-04-22 23:39:27 +0700

    Basic Validation

--- models/tasks.js
+++ models/tasks.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, 'Please Provide a Name'],
+    trim: true,
+    maxlength: [20, 'Name Cannot Be More Than 20 Characters'],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
