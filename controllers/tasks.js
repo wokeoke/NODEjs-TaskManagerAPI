5c79320e0d9fe76613ded643d3797f9d1d93b1fe
wokeoke <woke.oke@gmail.com>
2022-04-23 05:16:37 +0700

    Update Task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -33,8 +33,22 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send('update task');
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: `No Task With ID: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 const deleteTask = async (req, res) => {
