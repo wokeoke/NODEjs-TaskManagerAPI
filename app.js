fdbdc70cddf840fcd1459ebd8657195d5c6c20cd
wokeoke <woke.oke@gmail.com>
2022-04-22 21:52:52 +0700

    Basic Express Server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+// ROUTES
+app.get('/hello', (req, res) => res.send('Task Manager App'));
+
+const PORT = 3000;
+app.listen(PORT, console.log(`Server is listening on PORT ${PORT}`));
